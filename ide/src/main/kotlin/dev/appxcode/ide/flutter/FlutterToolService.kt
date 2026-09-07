208cef225303d4a6d44623d3b5471179c81698d5
Nguyễn Đức Tùng Lâm <50864894+LamPPKK@users.noreply.github.com>
2026-09-07 22:50:53 +0700

    add Flutter pub get action

--- ide/src/main/kotlin/dev/appxcode/ide/flutter/FlutterToolService.kt
+++ ide/src/main/kotlin/dev/appxcode/ide/flutter/FlutterToolService.kt
@@ -29,6 +29,7 @@ class FlutterToolService(
     fun sessionOutput(): String = sessionOutput.toString()
     fun run(root: Path, deviceId: String? = null): FlutterCommandResult = execute(root, "run", deviceId)
     fun test(root: Path): FlutterCommandResult = execute(root, "test", null)
+    fun pubGet(root: Path): FlutterCommandResult = execute(root, "pub", null)
     fun hotReload(root: Path): FlutterCommandResult = sendSignal(root, "r")
     fun hotRestart(root: Path): FlutterCommandResult = sendSignal(root, "R")
 
