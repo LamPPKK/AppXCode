db5e40db805ac17e46dcba3a0d9abb8bbbb3b8e6
Nguyễn Đức Tùng Lâm <50864894+LamPPKK@users.noreply.github.com>
2026-09-07 23:02:22 +0700

    allow resetting build cancellation

--- ide/src/main/kotlin/dev/appxcode/ide/build/BuildCancellation.kt
+++ ide/src/main/kotlin/dev/appxcode/ide/build/BuildCancellation.kt
@@ -5,5 +5,6 @@ import java.util.concurrent.atomic.AtomicBoolean
 class BuildCancellation {
     private val cancelled = AtomicBoolean(false)
     fun cancel() { cancelled.set(true) }
+    fun reset() { cancelled.set(false) }
     fun isCancelled(): Boolean = cancelled.get()
 }
