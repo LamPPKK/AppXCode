0b7128948f9948a93a59ad5501636ac00b38d9e7
Nguyễn Đức Tùng Lâm <50864894+LamPPKK@users.noreply.github.com>
2026-09-08 01:03:58 +0700

    dispose device registry with project lifecycle

--- ide/src/main/kotlin/dev/appxcode/ide/AppXCodeProjectService.kt
+++ ide/src/main/kotlin/dev/appxcode/ide/AppXCodeProjectService.kt
@@ -187,6 +187,7 @@ class AppXCodeProjectService(private val project: Project) : Disposable {
         (swiftLanguage as? AutoCloseable)?.close()
         swiftLanguage = null
         flutter.stopSession()
+        devices.clear()
         runConfigurations.clear()
         changeListeners.clear()
     }
