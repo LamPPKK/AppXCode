e6f869f5447be64ef0e6f64e6a6ad9182d12466b
Nguyễn Đức Tùng Lâm <50864894+LamPPKK@users.noreply.github.com>
2026-09-08 07:28:27 +0700

    disable refresh action after device registry close

--- ide/src/main/kotlin/dev/appxcode/ide/device/RefreshDevicesAction.kt
+++ ide/src/main/kotlin/dev/appxcode/ide/device/RefreshDevicesAction.kt
@@ -8,6 +8,7 @@ class RefreshDevicesAction : AnAction("Refresh Embedded Devices") {
         event.project?.getService(DeviceRegistryService::class.java)?.refresh()
     }
     override fun update(event: AnActionEvent) {
-        event.presentation.isEnabled = event.project != null
+        val service = event.project?.getService(DeviceRegistryService::class.java)
+        event.presentation.isEnabled = service != null && !service.isClosed()
     }
 }
